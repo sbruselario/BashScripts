bd3fc317c61c27441fdebef19f9a5f747f10a130
Sebastián R. Bruselario <sbruselario@frro.utn.edu.ar>
2016-02-28 20:08:26 -0300

    Cambios luego de actualizar a Ubuntu 16.04 LTS

--- Auto-configurar monitor/monitor_vga1.sh
+++ Auto-configurar monitor/monitor_vga1.sh
@@ -5,11 +5,13 @@
 #
 
 clear
-
+notify-send 'Esperando para acomodar pantallas...' ''
+sleep 10
 xrandr --newmode "1440x900_60.00"  106.50  1440 1528 1672 1904  900 903 909 934 -hsync +vsync 
 xrandr --addmode VGA1 1440x900_60.00
-xrandr --output VGA1 --primary
+xrandr --output VGA1 --primary --left-of LVDS1 #determino pantalla externa como principal y en la posición izquierda de notebook
+rm ~/.config/monitors.xml #con esto evito un mensaje de error al iniciar sesión
 
-notify-send 'MONITOR VGA1' ''
+notify-send 'MONITOR VGA1' 'configuración finalizada'
 
 exit 1
